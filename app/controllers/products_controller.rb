8de2efbb4eb4367bcfad724cde247dcbc752749f
karazhiya-o <karazhiya.sashka@gmail.com>
2018-05-30 13:20:58 +0300

    do it

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by_id(params[:id])
     #@order = Order.find_by_user_id(current_user.id)
-    @comments = Comment.where(:product_id => @product.id)
-    pp @comments
+    #@comments = Comment.where(:product_id => @product.id)
+    #pp @comments
 
 
   end
